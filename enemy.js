767d85381a4ea427049d7cb18d11c8322b55f31f
SunghyunJoo <70799336+SunghyunJoo@users.noreply.github.com>
2020-10-10 20:28:04 +0900

    통합본

---
+++ enemy.js
@@ -0,0 +1,30 @@
+class enemy{
+    constructor(x,y,tx,ty) {
+        this.pos = createVector(x,y);
+        this.targetPos = createVector(tx,ty);
+        this.direction = createVector(this.targetPos.x-this.pos.x,this.targetPos.y-this.pos.y);
+        this.angle=this.direction.heading();
+    }
+    update() {
+        this.direction = createVector(this.targetPos.x-this.pos.x,this.targetPos.y-this.pos.y);
+        let length =10;
+        if(this.direction.mag()<=length) {
+            this.pos.set(this.targetPos.x,this.targetPos.y);
+            this.angle=this.direction.heading()+HALF_PI;
+        }
+        else {
+            this.direction.setMag(length);
+            this.pos.add(this.direction);
+        }
+    }
+    display() {
+        if(second()%2)
+            fill(255,100,100);
+        else
+            fill(100,100,255);
+        circle(this.pos.x,this.pos.y,20);
+        let dx =this.pos.x + cos(this.angle)*20;
+        let dy =this.pos.y + sin(this.angle)*20;
+        line(this.pos.x,this.pos.y,dx,dy);
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,17 +1,20 @@
 var p
 var b
 var bullets = [];
-
+let enemyArray=[];
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(670, 570);
  // bullet = new Bullet(width/2,height/2);
-  
-p = new Player(width/2-30,350,50,550)
+  frameRate(30);
+  setInterval(function(){for(let i=0;i<8;i++)
+        setTimeout(function(){enemyArray.push(new enemy(0,0,(enemyArray.length%10)*width/12+width/12,(enemyArray.length-enemyArray.length%10)/10*height/10+height/10))},250*i);
+},4000);
+p = new Player(width/2-30,height*9/10,50,50);
 //p = new Player()
 }
 
 function draw() {
-  background(65);
+  background(50);
   
   p.draw()
   p.update();
@@ -20,9 +23,11 @@ function draw() {
       bullets[i].draw();
       bullets[i].move()
     }
-  
+  for(let i=0;i<enemyArray.length;i++) {
+        enemyArray[i].update();
+        enemyArray[i].display();
+    }
 }
-
 
   function keyPressed(){           
      if (keyCode === UP_ARROW) 
@@ -30,7 +35,4 @@ function draw() {
   var bullet1 = new Bullet(p.x+25, height-60);
       bullets.push(bullet1);
     } 
-   
-  
-  
     }
