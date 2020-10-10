bdbf3f46e6c25447da9d5c130a8d395252ec3740
SunghyunJoo <70799336+SunghyunJoo@users.noreply.github.com>
2020-10-10 20:07:31 +0900

    받은거 커밋

--- sketch.js
+++ sketch.js
@@ -1,6 +1,36 @@
+var p
+var b
+var bullets = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
+ // bullet = new Bullet(width/2,height/2);
+  
+p = new Player(width/2-30,350,50,550)
+//p = new Player()
+}
 
 function draw() {
-  background(220);
+  background(65);
+  
+  p.draw()
+  p.update();
+  for(var i=0; i<bullets.length; i++)
+    {
+      bullets[i].draw();
+      bullets[i].move()
+    }
+  
 }
+
+
+  function keyPressed(){           
+     if (keyCode === UP_ARROW) 
+     {
+  var bullet1 = new Bullet(p.x+25, height-60);
+      bullets.push(bullet1);
+    } 
+   
+  
+  
+    }
